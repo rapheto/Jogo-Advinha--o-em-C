0110ee8b2e6c9821346856fdde6b7aca5646e0cb
Raphael Miranda <rapheto@hotmail.com>
2025-01-10 16:02:01 -0300

    Add  if  and else for Chute
    criei as condições para saber se o chute foi verdadeiro ou falso e se ele foi maior ou menos

--- teste.c
+++ teste.c
@@ -7,4 +7,14 @@ int main(){
     scanf("%d", &chute);
     printf("Seu chute foi: %d", chute);
 
+    if(chute == numeroalt){
+        printf("Voce acertou!!");
+    } else{
+        printf("\nVoce errou!0");
+        if(chute > numeroalt){
+            printf("\nSeu chute foi maior do  que o numero alt");
+        } else{
+            printf("\nSeu chute foi menor do que o  mumero  alt");
+        }
+    }
 }
