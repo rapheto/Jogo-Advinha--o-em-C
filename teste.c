dc466021e022661b9cc218c1cf921e8cac5a3346
Raphael Miranda <rapheto@hotmail.com>
2025-01-10 15:51:43 -0300

    Add main
    Criei a função main e alguns prints

---
+++ teste.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int numeroalt = 42;
+    int chute;
+    printf("Digite um numero: ");
+    scanf("%d", &chute);
+    printf("Seu chute foi: %d", chute);
+
+}
