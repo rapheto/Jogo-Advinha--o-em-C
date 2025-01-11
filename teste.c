1042ade429c2710358471f9d01844df0bda7b154
Raphael Miranda <rapheto@hotmail.com>
2025-01-10 21:09:48 -0300

    Add For
    criei um loop for para que o usuario so tenha 3 tentativas de acerto

--- teste.c
+++ teste.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
 
+#define TENTATIVAS 3
 int main(){
+
+    printf("--------------------\n");
+    printf("-JOGO DA ADVINHACAO-");
+    printf("\n--------------------");
+
     int numeroalt = 42;
     int chute;
-    printf("Digite um numero: ");
-    scanf("%d", &chute);
-    printf("Seu chute foi: %d", chute);
 
-    if(chute == numeroalt){
-        printf("Voce acertou!!");
-    } else{
-        printf("\nVoce errou!0");
-        if(chute > numeroalt){
-            printf("\nSeu chute foi maior do  que o numero alt");
-        } else{
-            printf("\nSeu chute foi menor do que o  mumero  alt");
+    for(int i = 1; i <= TENTATIVAS; i++){
+        printf("\nVoce tem %d/%d tentativas", i, TENTATIVAS);
+        printf("\nDigite um numero: ");
+        scanf("%d", &chute);
+
+        //Logica (Bool)
+        int acertou = (chute == numeroalt);
+        int maior = (chute > numeroalt);
+
+        if(acertou){
+            printf("Seu chute foi: %d", chute);
+            printf("\nVoce acertou!!");
+            break;
+        } 
+        else if(maior){
+            printf("\nVoce chutou %d, chutou alto", chute);
+        } 
+        else{
+            printf("\nVoce chutou %d, chutou baixo", chute);
         }
+        printf("\nFim de Jogo!");
     }
+    
+    
 }
